72675c4e71c63e6b885aab20e88ab790761fe771
Bryan Paxton <bryan@starbelly.io>
2022-08-20 19:14:16 -0500

    Fix up release script

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-OTP_VER=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell | sed -r 's/\"([0-9]+)\"/\1/g')
+OTP_RAW_VER=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell)
+OTP_VER=$(echo ${OTP_RAW_VER} | sed -r 's/\"([0-9]+)\"/\1/g' | sed 's/[^0-9]*//g')
 
 yes_or_exit() {
     while true; do
@@ -13,16 +14,13 @@ yes_or_exit() {
     done
 }
 
-OTP_VER_STR="${OTP_VER}"
 if [ "${OTP_VER}" != "24" ]; then
      echo "You are using Erlang/OTP ${OTP_VER_STR}"
      echo "However, ex_doc must be built using OTP == 24 for compatability. Giving up..."
      exit
 fi
 
-exit
-
-VERSION=$1 
+VERSION=$1
 
 if [ "${VERSION}" == "" ]; then
     echo "Error: A version argument corresponding to a tag must be supplied"
@@ -34,15 +32,15 @@ mix deps.get
 mix escript.build
 rm -f priv/ex_doc
 
-mkdir -p  _checkouts 
+mkdir -p  _checkouts
 
-rm -rf _checkouts/rebar3_ex_doc 
+rm -rf _checkouts/rebar3_ex_doc
 
 cd _checkouts
 
-git clone https://github.com/starbelly/rebar3_ex_doc 
+git clone https://github.com/starbelly/rebar3_ex_doc
 
-cd rebar3_ex_doc 
+cd rebar3_ex_doc
 
 git checkout "v${VERSION}"
 
@@ -54,16 +52,16 @@ echo "Size of priv/ex_doc : $SIZE"
 
 rebar3 ex_doc
 
-echo 
+echo
 echo "Documentation has been generated for rebar3_ex_doc using rebar3_ex_doc."
-echo "This should be checked before publishing" 
-echo 
+echo "This should be checked before publishing"
+echo
 
 yes_or_exit "Open up the documentation?"
 
-open doc/index.html 
+open doc/index.html
 
-echo 
+echo
 yes_or_exit  "Did everything look ok?"
 
 rebar3 hex publish
