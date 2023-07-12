485a266dbf28aaecd8dbadd1fdc9626134abfb30
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-07-13 00:50:49 +0100

    Tweak language a bit

--- release.sh
+++ release.sh
@@ -8,22 +8,22 @@ yes_or_exit() {
         read -r -p "$1 " yn
         case $yn in
             [Yy][Ee][sS]) true; break ;;
-            [Nn][Oo]) echo "Ok, bye bye"; exit ;;
-            * ) echo "Please answer yes or no." ;;
+            [Nn][Oo]) echo "Ok, bye bye!"; exit ;;
+            * ) echo "Please answer 'yes' or 'no'." ;;
         esac
     done
 }
 
 if [ $((OTP_VER)) -lt 24 ]; then
-     echo "You are using Erlang/OTP ${OTP_VER}"
-     echo "However, ex_doc must be built using OTP > 24 for compatability. Giving up..."
+     echo "You are using Erlang/OTP ${OTP_VER}."
+     echo "However, ex_doc must be built using OTP > 24 for compatibility. Giving up..."
      exit
 fi
 
 VERSION=$1
 
 if [ "${VERSION}" == "" ]; then
-    echo "Error: A version argument corresponding to a tag must be supplied"
+    echo "Error: a version argument corresponding to a tag must be supplied!"
     exit
 fi
 
@@ -48,13 +48,13 @@ mix deps.get
 mix escript.build
 
 SIZE=$(du -ah priv/ex_doc | awk '{print $1}')
-echo "Size of priv/ex_doc : $SIZE"
+echo "Size of priv/ex_doc: $SIZE"
 
 rebar3 ex_doc
 
 echo
 echo "Documentation has been generated for rebar3_ex_doc using rebar3_ex_doc."
-echo "This should be checked before publishing"
+echo "This should be checked before publishing."
 echo
 
 yes_or_exit "Open up the documentation?"
