2415f08c31c75c6ef668dfe53c6fd6a543532f0c
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-07-22 21:00:43 +0100

    Tweak release.sh (#53)

--- release.sh
+++ release.sh
@@ -1,29 +1,29 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 OTP_RAW_VER=$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell)
-OTP_VER=$(echo ${OTP_RAW_VER} | sed -r 's/\"([0-9]+)\"/\1/g' | sed 's/[^0-9]*//g')
+OTP_VER=$(echo "${OTP_RAW_VER}" | sed -r 's/\"([0-9]+)\"/\1/g' | sed 's/[^0-9]*//g')
 
 yes_or_exit() {
     while true; do
         read -r -p "$1 " yn
         case $yn in
             [Yy][Ee][sS]) true; break ;;
-            [Nn][Oo]) echo "Ok, bye bye"; exit ;;
-            * ) echo "Please answer yes or no." ;;
+            [Nn][Oo]) echo "Ok, bye bye!"; exit ;;
+            * ) echo "Please answer 'yes' or 'no'." ;;
         esac
     done
 }
 
 if [ "${OTP_VER}" != "24" ]; then
-     echo "You are using Erlang/OTP ${OTP_VER_STR}"
-     echo "However, ex_doc must be built using OTP == 24 for compatability. Giving up..."
+     echo "You are using Erlang/OTP ${OTP_VER}."
+     echo "However, ex_doc must be built using OTP == 24 for compatibility. Giving up..."
      exit
 fi
 
 VERSION=$1
 
 if [ "${VERSION}" == "" ]; then
-    echo "Error: A version argument corresponding to a tag must be supplied"
+    echo "Error: a version argument corresponding to a tag must be supplied!"
     exit
 fi
 
@@ -47,14 +47,14 @@ git checkout "v${VERSION}"
 mix deps.get
 mix escript.build
 
-SIZE=$(ls -lh priv/ex_doc | awk '{print $5}')
-echo "Size of priv/ex_doc : $SIZE"
+SIZE=$(du -ah priv/ex_doc | awk '{print $1}')
+echo "Size of priv/ex_doc: $SIZE"
 
 rebar3 ex_doc
 
 echo
 echo "Documentation has been generated for rebar3_ex_doc using rebar3_ex_doc."
-echo "This should be checked before publishing"
+echo "This should be checked before publishing."
 echo
 
 yes_or_exit "Open up the documentation?"
