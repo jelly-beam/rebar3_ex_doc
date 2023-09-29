414f80930cbf591225e61d7cec9bc649e37dfab3
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-09-29 21:16:48 +0100

    Add Elixir script to bump ex_doc version from CLI (#67)
    * Prepare for AST update
    
    We simplify the file as much as possible so we don't have to
    deal with format changes when running Macro and Code stuff on
    top of it
    
    We also rename the app as rebar3_ex_doc (and the module as
    Rebar3ExDoc.MixProject), drop :start_permanent,
    :application (def), and :deps (defp), as well as comments
    
    * Allow up'ing our targeted ex_doc version from the CLI
    
    * Run mix deps.unlock ex_doc + mix deps.get automatically
    
    * Pin to an exact ex_doc version

---
+++ up_ex_doc_version.ex
@@ -0,0 +1,30 @@
+defmodule Mix.Tasks.UpExDocVersion do
+  use Mix.Task
+
+  @shortdoc "Update mix.exs' ex_doc version"
+  @mix_exs "mix.exs"
+
+  def run([new_vsn]) do
+    content_up =
+      @mix_exs
+      |> File.read!()
+      |> run(new_vsn)
+
+    File.write!(@mix_exs, "#{content_up}\n")
+    Mix.Task.run("deps.unlock", ["ex_doc"])
+    Mix.Task.run("deps.get")
+  end
+
+  defp run(mix_exs, new_vsn) do
+    mix_exs
+    |> Code.string_to_quoted!()
+    |> Macro.prewalk(fn
+      {:@, anno, [{:ex_doc_version, anno, [_cur_vsn]}]} ->
+        {:@, anno, [{:ex_doc_version, anno, [new_vsn]}]}
+
+      node ->
+        node
+    end)
+    |> Macro.to_string()
+  end
+end
