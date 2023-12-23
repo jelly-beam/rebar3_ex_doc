1cdf1e42cd3e85189cfdead92a3140fd6a80255c
Bryan Paxton <bryan@starbelly.io>
2023-12-23 12:13:36 -0600

    Pull pre-build ex_doc from github for releases
    This commit adjusts the release script to start pulling a pre-built
    ex-doc escript from elixir-lang's github
    (i.e., https://github.com/elixir-lang/ex_doc/releases/tag/v0.31.0).
    
    In addition, src, test, and mix.exs have been adjusted to reference and align with
    the escript name on ex-docs release artifiacts. Likewise, we also will now include
    the sha256sum in priv to be published along with the escript.

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
 defmodule Rebar3ExDoc.MixProject do
   use Mix.Project
-  @ex_doc_version "0.30.9"
+  @ex_doc_version "0.31.0"
   def project do
     [
       app: :rebar3_ex_doc,
       version: "0.2.21",
       elixir: "~> 1.13",
       deps: [ex_doc: "#{@ex_doc_version}"],
-      escript: [main_module: ExDoc.CLI, name: "ex_doc", path: "priv/ex_doc"],
+      escript: [main_module: ExDoc.CLI, name: "ex_doc_otp_24", path: "priv/ex_doc_otp_24"],
       docs: [main: "readme", extras: ["README.md"]]
     ]
   end
