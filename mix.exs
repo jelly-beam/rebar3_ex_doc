75b041919daeeee0ce86905c2ae8cd5d768c455e
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2025-05-28 17:29:06 -0500

    Bump version and update CHANGELOG

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rebar3ExDoc.MixProject do
   def project do
     [
       app: :rebar3_ex_doc,
-      version: "0.2.28",
+      version: "0.2.29",
       elixir: "~> 1.13",
       deps: [ex_doc: "#{@ex_doc_version}"],
       escript: [
